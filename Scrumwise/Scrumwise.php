a3b63164f513c1b824549150b342fda3d857f9d9
Peter van de Werken <pwerken@a-eskwadraat.nl>
2015-04-07 16:45:02 +0200

    remove init.php just include Scrumwise.php

--- Scrumwise/Scrumwise.php
+++ Scrumwise/Scrumwise.php
@@ -1,5 +1,16 @@
 <?php
 
+require_once 'ScrumwiseObject.php';
+require_once 'Taggable.php';
+require_once 'BacklogItem.php';
+require_once 'Project.php';
+require_once 'Release.php';
+require_once 'Sprint.php';
+require_once 'Tag.php';
+require_once 'Task.php';
+
+Scrumwise::configure(require 'config.php');
+
 class Scrumwise
 {
 	private static $user = NULL;
